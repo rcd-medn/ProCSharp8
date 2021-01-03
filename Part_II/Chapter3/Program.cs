8c4d1473aea4e3ff2946207d0a3ff93108d3affd
Ricardo Medina <para.sites.estudos@gmail.com>
2021-01-03 13:46:42 -0300

    Visto um pouco sobre entrada e saída de dados pelo console e formatação básica de strings.

--- Part_II/Chapter3/Program.cs
+++ Part_II/Chapter3/Program.cs
@@ -38,9 +38,11 @@ namespace Chapter3
                 Console.WriteLine("Arg: {0}", arg);
             }
 
-            // Chama o método ShowEnvironmentDetails() para imprimir algumas informações sobre o ambiente
-            // de execução atual que hospeda a aplicação.
             ShowEnvironmentDetails();
+
+            GetUserData();
+
+            FormatNumericalData();
 
             // O console espera até uma tecla qualquer seja pressiona.
             Console.ReadLine();
@@ -81,6 +83,8 @@ namespace Chapter3
         // static Task<int> Main(string[]) { }
         #endregion
 
+        // O método ShowEnvironmentDetails() imprimi algumas informações sobre o ambiente
+        // de execução atual que hospeda a aplicação.
         static void ShowEnvironmentDetails()
         {
             // Informa os discos rígidos locais que estão na máquina hospedeira da aplicação
@@ -93,6 +97,49 @@ namespace Chapter3
             Console.WriteLine("SO: {0}", Environment.OSVersion);
             Console.WriteLine("Número de Processadores: {0}", Environment.ProcessorCount);
             Console.WriteLine("Versão .NET Core: {0}", Environment.Version);
+        }
+
+        // Método criado para estudar o básico de entrada (input) e saída (output) da classe Console.
+        // Este exemplo está na seção The System.Console class do capítulo 3 - Chapter 3.
+        private static void GetUserData()
+        {
+            Console.WriteLine("\n\nMétodo: GetUserData()");
+            
+            // Solicita o nome e a idade.
+            Console.Write("Por favor, informe seu nome: ");
+            string userName = Console.ReadLine();
+
+            Console.Write("Por favor, informe sua idade: ");
+            string userAge = Console.ReadLine();
+
+            // Altera a cor do primeiro plano (letras) da linha de comando.
+            ConsoleColor prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Envia os dados obtidos para a saída padrão, linha de comando, no momento.
+            Console.WriteLine("Olá, {0}! Você está com {1} de idade!", userName, userAge);
+
+            // Restaura a cor da linha de comando.
+            Console.ForegroundColor = prevColor;
+        }
+
+        // Formata números utilizando caracteres de formatação dos placeholders.
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("\n\nMétodo: FormatNumericalData()");
+
+            Console.WriteLine("Formatando o número 99999 em diversos formatos:");
+            Console.WriteLine("Formato c: {0:c}", 99999);
+            Console.WriteLine("Formato d9: {0:d9}", 99999);
+            Console.WriteLine("Formato f3: {0:f3}", 99999);
+            Console.WriteLine("Formato n: {0:n}", 99999);
+            
+            // Reparar na caixa alta e baixa para hexadecimais.
+            // Determina se a letras estão em caixa alta ou baixa.
+            Console.WriteLine("Format E: {0:E}", 99999);
+            Console.WriteLine("Formato e: {0:e}", 99999);
+            Console.WriteLine("Formato X: {0:X}", 99999);
+            Console.WriteLine("Formato x: {0:x}", 99999);
         }
     }
 }
