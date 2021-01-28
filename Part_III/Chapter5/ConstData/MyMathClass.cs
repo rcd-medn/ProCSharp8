ccf517de5f08841a1cc055acb1d4143bb4feab8b
Ricardo <para.sites.estudos@gmail.com>
2021-01-28 17:25:06 -0300

    Finalizado o tópico sobre Dados Constantes - Working with Constant Field Data

---
+++ Part_III/Chapter5/ConstData/MyMathClass.cs
@@ -0,0 +1,15 @@
+
+
+
+
+
+namespace ConstData
+{
+    public class MyMathClass
+    {
+        // O valor de de um ponto de dado constante precisa ser atribuído no momento
+        // de sua definição!
+        // A atribuição não pode ser feita por construtores ou qualuqer tipo de método.
+        public const double PI = 3.14;
+    }
+}
---
+++ Part_III/Chapter5/ConstData/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+
+using System;
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trabalhando com campos de dados constantes");
+
+            Console.WriteLine("O valor de PI é: {0}", MyMathClass.PI);
+        }
+    }
+}
